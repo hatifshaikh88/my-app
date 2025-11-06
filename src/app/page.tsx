58a9f521341bb0d41e8663c0d240201b7eb54157
hatifshaikh88 <hatifshaikh88732@gmail.com>
2025-11-07 00:18:01 +0530

    1: Set Up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <Button >
-        Hello World 
-      </Button>
+    <div className="p-4">
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button>
+          I'm a button
+        </Button>
+      </div>
+      <div>
+        <Input placeholder="I'm an Input" />
+      </div>
+      <div>
+        <Progress value={50}  />
+      </div>
+      <div>
+        <Textarea placeholder="I'm a Textarea" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
+    </div>
     </div>
   );
 }
